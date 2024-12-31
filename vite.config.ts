4eaa1a7cc2e811f04f5525073a078ec8622186f6
Kofi Ofori Edoo <kofiedoo@Mac.localdomain>
2024-12-31 10:04:52 +0000

    Frontend Starter

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vitest/config'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [],
+  test:{
+    globals:true,
+    environment:"jsdom"
+  }
+})
