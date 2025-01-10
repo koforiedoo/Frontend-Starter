88f371d11e1904637c748f64e4cab0f49c41b627
Kofi Ofori Edoo <kofiedoo@Kofis-MacBook-Pro.local>
2025-01-10 21:38:33 +0000

    Apollo client setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
-function App() {
+import { gql, useQuery } from "@apollo/client"
 
+const GET_BIRDS = gql`
+  query GetBirds{
+    getBirds{
+      commonName
+      scientificName
+    }
+  }
+`
+function App() {
+  const {loading, error, data } = useQuery(GET_BIRDS)
+  console.log(data)
   return (
     <h1>Hello, World!</h1>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: import.meta.env.VITE_GRAPHQL_SERVER as string,
+  cache: new InMemoryCache(),
+});
+
+createRoot(document.getElementById("root")!).render(
+  <ApolloProvider client={client}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </ApolloProvider>
+);
